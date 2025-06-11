1ae85302b4fe1379f46cbf3060967b6f8acbc69a
LuciusBest <paulpaturel75@gmail.com>
2025-06-11 15:28:29 +0200

    Fix grid and shader logic

--- js/ui/app.js
+++ js/ui/app.js
@@ -14,8 +14,28 @@ const archives = [
     archive: 'ARCHIVE_003_data.json',
   },
   {
+    file: 'videos/Archive_004_ThatsNotMe_All-StarRemix_LiveOnToddlaT.mp4',
+    archive: 'ARCHIVE_004_data.json',
+  },
+  {
+    file: 'videos/Archive_005_KeepinItGrimy_Sessions_feat_MerkyAce_MIK_Ego.mp4',
+    archive: 'ARCHIVE_005_data.json',
+  },
+  {
+    file: 'videos/Archive_006_LoganSama_MerkyACE_Footsie_TKO_ShifMan_on_Kiss_Sept_5th_2011.mp4',
+    archive: 'ARCHIVE_006_data.json',
+  },
+  {
     file: 'videos/Archive_007_TempaT_Skepta_JME_LoganSama.mp4',
     archive: 'ARCHIVE_007_data.json',
+  },
+  {
+    file: 'videos/Archive_008_Scrufizzer_2Face_Teeza_15th_November_2011.mp4',
+    archive: 'ARCHIVE_008_data.json',
+  },
+  {
+    file: 'videos/Archive_009_Trilla_crew_Crib_Session_Part_2TimWestwoodTV.mp4',
+    archive: 'ARCHIVE_009_data.json',
   },
 ];
 
